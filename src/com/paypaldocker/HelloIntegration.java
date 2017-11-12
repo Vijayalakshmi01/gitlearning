5902464dee685436560ef597a2b48bb284104790
Shanmugam <vishanmugam@paypal.com>
2017-11-12 20:04:02 +0530

    Exploring Egit

--- src/com/paypaldocker/HelloIntegration.java
+++ src/com/paypaldocker/HelloIntegration.java
@@ -5,6 +5,7 @@ public class HelloIntegration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of ETS-Integrations!!!!");
 		System.out.println("Testing team - Viji and Hari");
+		System.out.println("Checking from Egit");
 	}
 
 }
